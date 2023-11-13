047cc91fb086dd35667483394c21d842c3e244cd
Martan03 <martinslezak03@gmail.com>
2023-11-13 15:06:01 +0100

    Add testing solution 2

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int main(void) {
     char login[] = "vitejte-v-inp-2023";
-    int v0, v1, a0, a1, t0, a2, a3, t1, t2, t3, t5;
-    char s0, s1, s2;
+    int v0, v1, a0, a1, t0, a2, a3, t1, t2, t3, t4, t5;
+    char s0, s1, s2, s3, s4, s5;
     int zero = 0;
 
         a3 = zero + 1; // daddi $a3, $zero, 1
@@ -23,24 +23,26 @@ int main(void) {
         goto loop_first; // j loop_first
 
 insert_inner_end1:
+        // ; t5 == v0 no need to swap
         a3 = v0 + 0; // daddi $a3, $v0, 0
-        v0 = v0 + 1; // daddi $v0, $v0, 1
+        v1 = v0 + 1; // daddi $v1, $v0, 1
         login[t5] = s0; // sb $s0, login($t5)
-        s2 = s0 + 0; // daddi $s2, $s0, 0
-        s0 = login[v0]; // lb $s0, login($v0)
         t1 = a3 + -1; // daddi $t1, $a3, -1
-        // ; nop
-        if (s0 != 0) goto loop; // bnez $s0, loop
+        s3 = login[v1]; // lb $s3, login($v1)
+        s1 = login[a3]; // lb $s1, login($a3)
+        s2 = login[t1]; // lb $s2, login($t1)
+        if (s3 != 0) goto loop1_first; // bnez $s3, loop1_first
         goto insert_end; // j insert_end
 
 insert_inner_end2:
-        v0 = v0 + 1; // daddi $v0, $v0, 1
+        v1 = v0 + 1; // daddi $v1, $v0, 1
         login[a3] = s0; // sb $s0, login($a3)
-        // ; nop
-        s0 = login[v0]; // lb $s0, login($v0)
-        t1 = v0 + -2; // daddi $t1, $v0, -2
-        a3 = v0 + -1; // daddi $a3, $v0, -1
-        if (s0 == 0) goto insert_end; // beqz $s0, insert_end
+        // nop
+        s3 = login[v1]; // lb $s3, login($v1)
+        a3 = v1 + -1; // daddi $a3, $v1, -1
+        t1 = v1 + -2; // daddi $t1, $v1, -2
+        if (s3 != 0) goto loop1; // bnez $s3, loop1
+        goto insert_end; // j insert_end
 
 loop:
         s1 = login[a3]; // lb $s1, login($a3)
@@ -57,26 +59,75 @@ loop_first:
         a3 = a3 + -2; // daddi $a3, $a3, -2
         if (t1 >= 0) goto loop; // bgez $t1, loop
 
-insert_inner_last:
+        v1 = v0 + 1; // daddi $v1, $v0, 1
         t5 = t1 + 2; // daddi $t5, $t1, 2
-        if (a3 == t2) goto insert_inner_end; // beq $a3, $t2, insert_inner_end
-        s1 = login[a3]; // lb $s1, login($a3)
-        a1 = s0 < s1; // sltu $a1, $s0, $s1
-        // ; nop
-        // ; nop
-        if (a1 == 0) goto insert_inner_end; // beqz $a1, insert_inner_end
-        login[t5] = s1; // sb $s1, login($t5)
-        t5 = t5 + -1; // daddi $t5, $t5, -1
-
-insert_inner_end:
         a3 = v0 + 0; // daddi $a3, $v0, 0
-        v0 = v0 + 1; // daddi $v0, $v0, 1
+        s3 = login[v0]; // lb $s3, login($v0)
         login[t5] = s0; // sb $s0, login($t5)
+        t1 = v0 + -1; // daddi $t1, $v0, -1
+        if (s3 != 0) goto loop1; // bnez $s3, loop1
+        goto insert_end; // j insert_end
+
+loop1_inner_end1:
+        // ; t5 == v0 no need to swap
+        a3 = v1 + 0; // daddi $a3, $v1, 0
+        v0 = v1 + 1; // daddi $v0, $v1, 1
+        login[t5] = s3; // sb $s3, login($t5)
         t1 = a3 + -1; // daddi $t1, $a3, -1
         s0 = login[v0]; // lb $s0, login($v0)
-        // ; nop
-        // ; nop
+        s1 = login[a3]; // lb $s1, login($a3)
+        s2 = login[t1]; // lb $s2, login($t1)
+        if (s0 != 0) goto loop_first; // bnez $s0, loop_first
+        goto insert_end; // j insert_end
+
+loop1_inner_end2:
+        v0 = v1 + 1; // daddi $v0, $v1, 1
+        login[a3] = s3; // sb $s3, login($a3)
+        // nop
+        s0 = login[v0]; // lb $s0, login($v0)
+        a3 = v0 + -1; // daddi $a3, $v0, -1
+        t1 = v0 + -2; // daddi $t1, $v0, -2
         if (s0 != 0) goto loop; // bnez $s0, loop
+        goto insert_end; // j insert_end
+
+loop1:
+        s1 = login[a3]; // lb $s1, login($a3)
+        s2 = login[t1]; // lb $s2, login($t1)
+loop1_first:
+        t5 = a3 + 1; // daddi $t5, $a3, 1
+        a1 = s3 < s1; // sltu $a1, $s3, $s1
+        a2 = s3 < s2; // sltu $a2, $s3, $s2
+        t1 = t1 + -2; // daddi $t1, $t1, -2
+        if (a1 == 0) goto loop1_inner_end1; // beqz $a1, loop1_inner_end1
+        login[t5] = s1; // sb $s1, login($t5)
+        if (a2 == 0) goto loop1_inner_end2; // beqz $a2, loop1_inner_end2
+        login[a3] = s2; // sb $s2, login($a3)
+        a3 = a3 + -2; // daddi $a3, $a3, -2
+        if (t1 >= 0) goto loop1; // bgez $t1, loop1
+
+insert_inner_last:
+        t5 = t1 + 2; // daddi $t5, $t1, 2
+        s1 = login[a3]; // lb $s1, login($a3)
+        a3 = v1 + 0; // daddi $a3, $v1, 0
+        v0 = v1 + 1; // daddi $v0, $v1, 1
+        a1 = s0 < s1; // sltu $a1, $s0, $s1
+        t4 = t5 + -1; // daddi $t4, $t5, -1
+        t1 = v0 + -2; // daddi $t1, $v0, -2
+        if (a1 == 0) goto insert_inner_last_end; // beqz $a1, insert_inner_last_end
+        login[t5] = s1; // sb $s1, login($t5)
+        login[t4] = s3; // sb $s3, login($t4)
+        s0 = login[v0]; // lb $s0, login($v0)
+        s1 = login[a3]; // lb $s1, login($a3)
+        s2 = login[t1]; // lb $s2, login($t1)
+        if (s0 != 0) goto loop_first; // bnez $s0, loop_first
+        goto insert_end; // j insert_end
+
+insert_inner_last_end:
+        login[t5] = s3; // sb $s3, login($t5)
+        s0 = login[v0]; // lb $s0, login($v0)
+        s1 = login[a3]; // lb $s1, login($a3)
+        s2 = login[t1]; // lb $s2, login($t1)
+        if (s0 != 0) goto loop_first; // bnez $s0, loop_first
 
 insert_end:
 
--- xsleza26.s
+++ xsleza26.s
@@ -1,8 +1,8 @@
 ; Autor reseni: Martin Slezák xsleza26
-; Pocet cyklu k serazeni puvodniho retezce: 965
-; Pocet cyklu razeni sestupne serazeneho retezce: 1295
-; Pocet cyklu razeni vzestupne serazeneho retezce: 254
-; Pocet cyklu razeni retezce s vasim loginem: 251
+; Pocet cyklu k serazeni puvodniho retezce: 838
+; Pocet cyklu razeni sestupne serazeneho retezce: 1121
+; Pocet cyklu razeni vzestupne serazeneho retezce: 270
+; Pocet cyklu razeni retezce s vasim loginem: 235
 ; Implementovany radici algoritmus: Insertion sort
 ; ------------------------------------------------
 
@@ -21,87 +21,92 @@ params_sys5:    .space  8   ; misto pro ulozeni adresy pocatku
 ; CODE SEGMENT
                 .text
 main:
-        ; Inits variables and handles first two characters
-        daddi $t0, $zero, 1
-        lb $s1, login($zero)
-        daddi $t3, $zero, -1
-        lb $s0, login($t0)
-        ; Checks if char is zero character
-        beqz $s1, insert_end
+        daddi $a3, $zero, 1
         daddi $v0, $zero, 2
-        sltu $a0, $s0, $s1
-        ; Checks if char is zero character
-        beqz $s0, insert_end
-        daddi $a2, $t0, 1
-        ; Skips swapping if latter char is not less
-        beqz $a0, init
-        ; Swaps first two characters
-        sb $s1, login($t0)
-        sb $s0, login($zero)
-
-; Inits for main loop
-init:
+        lb $s1, login($zero)
+        lb $s2, login($a3)
+        daddi $t2, $zero, -1
+        beqz $s1, insert_end
         lb $s0, login($v0)
-        lb $s1, login($t0)
+        sltu $a1, $s2, $s1
         daddi $t1, $zero, 0
-        beqz $s0, insert_end
+        beqz $s2, insert_end
+        beqz $a1, loop
+        sb $s1, login($a3)
+        sb $s2, login($zero)
 
-; Main loop loads two chars at the time
-; It uses insertion sort
-; When char is smaller or equal to char on $v0, jumps to insert_inner_end
-; This cycle is repeated until there are at least two chars to swap
-; When not enough chars, continues to code which swaps last two
-insert:
-        sltu $a0, $s0, $s1
-        lb $s2, login($t1)
-        daddi $t1, $t1, -1
-        beqz $a0, insert_inner_end
-        sltu $a1, $s0, $s2
+        j loop_first
 
-        sb $s1, login($a2)
-        daddi $a2, $a2, -1
-        beqz $a1, insert_inner_end
-
-        nop
-        sb $s2, login($a2)
-        daddi $a2, $a2, -1
-        beq $t1, $t3, insert_inner_end
-        lb $s1, login($t1)
-        daddi $t1, $t1, -1
-        bne $a2, $t0, insert
-
-        ; Swaps first character with character $a0 from loop
-        ; Inits variables for next loop
-        sltu $a0, $s0, $s1
-        daddi $a2, $v0, 1
-        sb $s0, login($t0)
-        ; If it's not supposed to swap, jump to insert_inner_end1
-        beqz $a0, insert_inner_end1
-        sb $s1, login($t0)
-        sb $s0, login($zero)
-
-; Inits variables for next loop
-; Jumps to insert loop if next loaded char is not zero char
 insert_inner_end1:
-        lb $s1, login($v0)
-        lb $s0, login($a2)
+        ; t5 == v0 no need to swap
+        daddi $a3, $v0, 0
         daddi $v0, $v0, 1
-        daddi $t1, $a2, -2
-        bnez $s0, insert
+        sb $s0, login($t5)
+        daddi $t1, $a3, -1
+        lb $s0, login($v0)
+        lb $s1, login($a3)
+        lb $s2, login($t1)
+        bnez $s0, loop_first
         j insert_end
 
-; Saves char in $s0 to $a2
-; Inits variables for next loop
-; Jumps to insert loop if next loaded char is not zero char
-insert_inner_end:
-        daddi $t1, $v0, 0
+insert_inner_end2:
         daddi $v0, $v0, 1
-        sb $s0, login($a2)
-        daddi $a2, $t1, 1
+        sb $s0, login($a3)
+        nop
         lb $s0, login($v0)
-        lb $s1, login($t1)
-        daddi $t1, $t1, -1
-        bnez $s0, insert
+        daddi $a3, $v0, -1
+        daddi $t1, $v0, -2
+        beqz $s0, insert_end
+
+loop:
+        lb $s1, login($a3)
+        lb $s2, login($t1)
+loop_first:
+        daddi $t5, $a3, 1
+        sltu $a1, $s0, $s1
+        sltu $a2, $s0, $s2
+        daddi $t1, $t1, -2
+        beqz $a1, insert_inner_end1
+        sb $s1, login($t5)
+        beqz $a2, insert_inner_end2
+        sb $s2, login($a3)
+        daddi $a3, $a3, -2
+        bgez $t1, loop
+
+insert_inner_last:
+        daddi $t5, $t1, 2
+        beq $a3, $t2, insert_inner_end
+        lb $s1, login($a3)
+        daddi $a3, $v0, 0
+        daddi $v0, $v0, 1
+        sltu $a1, $s0, $s1
+        daddi $t4, $t5, -1
+        daddi $t1, $v0, -2
+        beqz $a1, insert_inner_last_end
+        sb $s1, login($t5)
+        sb $s0, login($t4)
+        lb $s0, login($v0)
+        lb $s1, login($a3)
+        lb $s2, login($t1)
+        bnez $s0, loop_first
+        j insert_end
+
+insert_inner_last_end:
+        sb $s0, login($t5)
+        lb $s0, login($v0)
+        lb $s1, login($a3)
+        lb $s2, login($t1)
+        bnez $s0, loop_first
+        j insert_end
+
+insert_inner_end:
+        daddi $v0, $v0, 1
+        nop
+        sb $s0, login($t5)
+        lb $s0, login($v0)
+        daddi $a3, $v0, -1
+        daddi $t1, $v0, -2
+        bnez $s0, loop
 
 insert_end:
 
